1bde1e1da5d613675bed654e7970769adeb01f7c
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-02-17 02:21:09 -0800

    Add common hooks file

---
+++ src/hooks.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react'
+
+const useFetch = (url, initialData, options = {}) => {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState(initialData)
+
+  useEffect(() => {
+    setLoading(true)
+
+    async function doFetch () {
+      try {
+        const response = await fetch(url, options)
+        if (response.ok) {
+          const json = await response.json()
+          setData(json)
+        } else {
+          setData(null)
+        }
+      } catch (error) {
+        setData(error)
+      }
+    }
+
+    doFetch()
+    setLoading(false)
+  }, [url])
+
+  return { loading, result: data }
+}
+
+const useLocalStorage = (key, initialValue) => {
+  const [item, setInnerValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      return initialValue
+    }
+  })
+
+  const setValue = value => {
+    setInnerValue(value)
+    window.localStorage.setItem(key, JSON.stringify(item))
+  }
+
+  return [item, setValue]
+}
+
+export { useFetch, useLocalStorage }
