8fbb574a5c54ecd386928d88bc9777629f1e19e3
Conrad Chase Metzger <cmetzger@bearcubs.santarosa.edu>
2019-02-23 03:43:23 -0800

    Sync to backend change of bio-link -> bio_link in correct answer response

--- src/CorrectAnswer.js
+++ src/CorrectAnswer.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Card from 'react-bootstrap/Card'
 
-export default function CorrectAnswer ({ quote }) {
+export default function CorrectAnswer({ quote }) {
   return (
     <Card className="mx-auto" style={{ width: '30rem', marginTop: '5rem' }}>
       <Card.Body>
         <Jumbotron>
           <h1>The answer was {quote.author}</h1>
-          <p>Learn more about this person: <a href={quote['bio-link']} rel="noopener noreferrer" target="_blank">{quote['bio-link']}</a></p>
+          <p>Learn more about this person: <a href={quote['bio_link']} rel="noopener noreferrer" target="_blank">{quote['bio-link']}</a></p>
         </Jumbotron>
       </Card.Body>
     </Card>
